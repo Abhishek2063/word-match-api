43503d0b6d10ac29c730496362e2bc153affc480
abhishek <abhishekgarg2063@gmail.com>
2024-03-20 23:26:42 +0530

    added the cors

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 require("dotenv").config();
 const searchRoutes = require('./routes/searchRoutes');
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
 
 // Middleware for parsing JSON and x-www-form-urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors(corsOpts));
 
 // API routes
 app.use('/search', searchRoutes);
